760a29332374848dfb42e89ecb16a8354ea23573
xyny <60004820+xynydev@users.noreply.github.com>
2024-11-10 16:07:21 +0000

    chore: wording changes, typo fix

--- build_opts_check.sh
+++ build_opts_check.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 # Expected env vars:
-# SQUASH_INPUT_VALUER
+# SQUASH_INPUT_VALUE
 # BUILD_OPTS
 
 if [ "$SQUASH_INPUT_VALUE" != "true" ]; then
   if grep -qE '(-B)|(--build-driver)' <<< "$BUILD_OPTS"; then
-    echo 'Cannot provide --build-driver in build_opts while squash = true'
+    echo 'Cannot provide --build-driver in build_opts while squash is set to true.'
     exit 1
   fi
 
   if grep -qE '(-s)|(--squash)' <<< "$BUILD_OPTS"; then
-    echo 'Cannot provide --squash in build_opts while squash = true'
+    echo 'Cannot provide --squash in build_opts while squash is set to true.'
     exit 1
   fi
 fi
 
 if grep -qE '(-p)|(--push)' <<< "$BUILD_OPTS"; then
-  echo 'Please do not provide --push in build_opts as the action provides it' \
-    ' by default anyway.'
+  echo 'Please do not add --push to build_opts, as the action already provides that argument.'
   exit 1
 fi
 
