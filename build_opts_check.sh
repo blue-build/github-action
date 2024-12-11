3db3b3b47b793b1c1f69ba1555e81ef240749265
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-11 16:35:46 +0100

    fix: Check for user-provided `build_opts`

--- build_opts_check.sh
+++ build_opts_check.sh
@@ -6,18 +6,18 @@ set -euo pipefail
 # BUILD_OPTS
 
 if [ "$SQUASH_INPUT_VALUE" != "true" ]; then
-  if grep -qE '(-B)|(--build-driver)' <<< "$BUILD_OPTS"; then
+  if [ -n "$(awk '/(^|\s)(-B|--build-driver)($|\s)/' <<< "$BUILD_OPTS")" ]; then
     echo 'Cannot provide --build-driver in build_opts while squash is set to true.'
     exit 1
   fi
 
-  if grep -qE '(-s)|(--squash)' <<< "$BUILD_OPTS"; then
+  if [ -n "$(awk '/(^|\s)(-s|--squash)($|\s)/' <<< "$BUILD_OPTS")" ]; then
     echo 'Cannot provide --squash in build_opts while squash is set to true.'
     exit 1
   fi
 fi
 
-if grep -qE '(-p)|(--push)' <<< "$BUILD_OPTS"; then
+if [ -n "$(awk '/(^|\s)(-p|--push)($|\s)/' <<< "$BUILD_OPTS")" ]; then
   echo 'Please do not add --push to build_opts, as the action already provides that argument.'
   exit 1
 fi
