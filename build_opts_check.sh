3647178beb6ca9924520f48d4572c8aa8d8c1adc
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-11 17:04:46 +0100

    chore: Simplify checking for build_opts

--- build_opts_check.sh
+++ build_opts_check.sh
@@ -5,21 +5,31 @@ set -euo pipefail
 # SQUASH_INPUT_VALUE
 # BUILD_OPTS
 
-if [ "$SQUASH_INPUT_VALUE" != "true" ]; then
-  if [ -n "$(awk '/(^|\s)(-B|--build-driver)($|\s)/' <<< "$BUILD_OPTS")" ]; then
-    echo 'Cannot provide --build-driver in build_opts while squash is set to true.'
-    exit 1
-  fi
+# check_build_opts option1 option2 error_message
+# If you have only 1 option to provide, provide '---' string as 2nd placeholder option
+check_build_opts() {
+    local option1="${1}"
+    local option2="${2}"
+    local error_message="${3}"
 
-  if [ -n "$(awk '/(^|\s)(-s|--squash)($|\s)/' <<< "$BUILD_OPTS")" ]; then
-    echo 'Cannot provide --squash in build_opts while squash is set to true.'
-    exit 1
-  fi
+    if [[ "${2}" == "---" ]]; then
+      if [[ -n "$(awk '/(^|\s)('${option1}')($|\s)/' <<< "${BUILD_OPTS}")" ]]; then
+        echo "${error_message}"
+        exit 1
+      fi
+    elif [[ -n "${2}" ]]; then
+      if [[ -n "$(awk '/(^|\s)('${option1}'|'${option2}')($|\s)/' <<< "${BUILD_OPTS}")" ]]; then
+        echo "${error_message}"
+        exit 1
+      fi
+    fi
+}
+
+if [[ "$SQUASH_INPUT_VALUE" != "true" ]]; then
+  check_build_opts "-B" "--build-driver" "Cannot provide '--build-driver' in build_opts while 'squash' is set to true."
+  check_build_opts "-s" "--squash" "Cannot provide '--squash' in build_opts while 'squash' is set to true."
 fi
 
-if [ -n "$(awk '/(^|\s)(-p|--push)($|\s)/' <<< "$BUILD_OPTS")" ]; then
-  echo 'Please do not add --push to build_opts, as the action already provides that argument.'
-  exit 1
-fi
+check_build_opts "-p" "--push" "Please do not add '--push' to build_opts, as the action already provides that argument."
 
 exit 0
